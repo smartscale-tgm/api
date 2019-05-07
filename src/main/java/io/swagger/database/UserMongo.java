dbc01d565f5a3fd638ba2b393db0322aa94c0c58
David Langheiter <david@langheiter.com>
2019-05-07 20:07:56 +0200

    Added id and role to User

--- src/main/java/io/swagger/database/UserMongo.java
+++ src/main/java/io/swagger/database/UserMongo.java
@@ -1,5 +1,6 @@
 package io.swagger.database;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,9 +9,12 @@ import org.threeten.bp.LocalDate;
 @Document(collection = "users")
 public class UserMongo {
 
+    @Id
+    private String id;
     @Indexed(unique = true)
     private String email;
     private String password;
+    private String role;
     private String name;
     private Float height;
     private LocalDate birthday;
@@ -21,9 +25,18 @@ public class UserMongo {
     public UserMongo(String email, String password, String name, Float height, LocalDate birthday) {
         this.email = email;
         this.password = password;
+        this.role = "user";
         this.name = name;
         this.height = height;
         this.birthday = birthday;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getEmail() {
@@ -40,6 +53,14 @@ public class UserMongo {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getName() {
